9d4ae7ea617f53e5f8c238d014af585756567578
Dhanushkumar <dhanushsrs@gmail.com>
2024-04-01 15:10:22 +0000

    Coding Practice 3b Technology Cards

---
+++ src/components/CardItem/index.js
@@ -0,0 +1,17 @@
+// Write your code here.
+import './index.css'
+
+const CardItem = props => {
+  const {cardDetails} = props
+  const {title, description, imgUrl, className} = cardDetails
+
+  return (
+    <li className={className}>
+      <h1 className="card-heading">{title}</h1>
+      <p className="card-description">{description}</p>
+      <img src={imgUrl} alt={title} className="image-size" />
+    </li>
+  )
+}
+
+export default CardItem
